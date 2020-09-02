625d2c6f824abe71336198c6c9032d46793ba35b
Natasha Singh <nasi5167@gmail.com>
2020-09-02 21:01:31 +0200

    update footer
    footer changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2019 Natasha Singh</li>
+              <li>&copy; Copyright 2020 Natasha Singh</li>
               {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
